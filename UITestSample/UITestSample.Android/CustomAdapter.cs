7fcece4b138aecfc2ebe40be9f737b1ae3a0296a
anaselhajjaji <elhajjaji.anas@gmail.com>
2016-03-05 11:13:12 +0000

    Add UI Test sample for Android

---
+++ UITestSample/UITestSample.Android/CustomAdapter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Android.Widget;
+using Android.Views;
+using Android.Util;
+using Android.Support.V7.Widget;
+
+namespace UITestSample.Android
+{
+    public class CustomAdapter : RecyclerView.Adapter
+    {
+        public const string TAG = "CustomAdapter";
+
+        public List<String> DataSet { get; set; }
+
+        // Provide a reference to the type of views that you are using (custom ViewHolder)
+        public class ViewHolder : RecyclerView.ViewHolder
+        {
+            private TextView textView;
+
+            public TextView TextView
+            {
+                get{ return textView; }
+            }
+
+            public ViewHolder(View v)
+                : base(v)
+            {
+                textView = (TextView)v.FindViewById(Resource.Id.textView);
+            }
+        }
+
+        // Initialize the dataset of the Adapter
+        public CustomAdapter(List<String> dataSet)
+        {
+            this.DataSet = dataSet;
+        }
+
+        // Create new views (invoked by the layout manager)
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup viewGroup, int position)
+        {
+            View v = LayoutInflater.From(viewGroup.Context)
+                .Inflate(Resource.Layout.row_item, viewGroup, false);
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        public override void OnBindViewHolder(RecyclerView.ViewHolder viewHolder, int position)
+        {
+            Log.Debug(TAG, "\tElement " + position + " set.");
+
+            // Get element from your dataset at this position and replace the contents of the view
+            // with that element
+            (viewHolder as ViewHolder).TextView.SetText(DataSet[position], TextView.BufferType.Normal);
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        public override int ItemCount
+        {
+            get{ return DataSet.Count; }
+        }
+    }
+}
+
---
+++ UITestSample/UITestSample.Android/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.Widget;
+using Android.Views;
+
+namespace UITestSample.Android
+{
+    [Activity(Label = "UITestSample.Android", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        private RecyclerView recyclerView;
+        private RecyclerView.Adapter adapter;
+        private RecyclerView.LayoutManager layoutManager;
+        private List<string> dataSet = new List<string>();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            recyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+
+            // A LinearLayoutManager is used here, this will layout the elements in a similar fashion
+            // to the way ListView would layout elements. The RecyclerView.LayoutManager defines how the
+            // elements are laid out.
+            layoutManager = new LinearLayoutManager(this);
+            recyclerView.SetLayoutManager(layoutManager);
+
+            adapter = new CustomAdapter(dataSet);
+            // Set CustomAdapter as the adapter for RecycleView
+            recyclerView.SetAdapter(adapter);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu) {
+            MenuInflater.Inflate(Resource.Menu.menu, menu);
+            return true;
+        }
+            
+        public override bool OnOptionsItemSelected(IMenuItem item) {
+            // Handle item selection
+            switch (item.ItemId) {
+                case Resource.Id.addAction:
+                    dataSet.Add("new item");
+                    adapter.NotifyItemInserted(dataSet.Count - 1);
+                    return true;
+                default:
+                    return OnOptionsItemSelected(item);
+            }
+        }
+    }
+}
+
+
