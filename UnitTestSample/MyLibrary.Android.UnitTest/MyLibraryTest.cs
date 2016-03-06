be0e1fa1d1f2caf027cd9efb8e49cbca7949e8b9
anaselhajjaji <elhajjaji.anas@gmail.com>
2016-03-06 17:22:47 +0000

    Add Unit Test Sample

---
+++ UnitTestSample/MyLibrary.Android.UnitTest/MyLibraryTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+
+using MyLibrary.Android;
+
+namespace UnitTestSample.Android
+{
+    [TestFixture]
+    public class TestsSample
+    {
+        MyLibraryClass myLib;
+
+        [SetUp]
+        public void Setup()
+        {
+            myLib = new MyLibraryClass();
+        }
+        
+        [TearDown]
+        public void Tear()
+        {
+            myLib.Dispose();
+        }
+
+        [Test]
+        public void Pass()
+        {
+            Assert.True(myLib.ShouldReturnTrue());
+        }
+
+        [Test]
+        public void Fail()
+        {
+            Assert.True(myLib.ShouldReturnFalse());
+        }
+
+        [Test]
+        [Ignore("Ignored test for some reason")]
+        public void Ignore()
+        {
+            myLib.ShouldReturnFalse();
+        }
+    }
+}
+
---
+++ UnitTestSample/MyLibrary.Android/MyLibraryClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyLibrary.Android
+{
+    public class MyLibraryClass : IDisposable
+    {
+        public MyLibraryClass()
+        {
+        }
+
+        public bool ShouldReturnTrue()
+        {
+            return true;
+        }
+
+        public bool ShouldReturnFalse()
+        {
+            return false;
+        }
+
+        public string ReturnSameValue(string value)
+        {
+            return value;
+        }
+
+        #region IDisposable implementation
+
+        public void Dispose()
+        {
+            // Free resources here
+        }
+
+        #endregion
+    }
+}
+
