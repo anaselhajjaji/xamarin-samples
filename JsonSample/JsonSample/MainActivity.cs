00601ba97d03d35a79ffd060410368661f544629
anaselhajjaji <elhajjaji.anas@gmail.com>
2016-03-17 10:47:24 +0000

    Improve the sample

--- JsonSample/JsonSample/MainActivity.cs
+++ JsonSample/JsonSample/MainActivity.cs
@@ -26,12 +26,11 @@ namespace JsonSample
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
+            // Initialize the recycler view
             recyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
             layoutManager = new LinearLayoutManager(this);
             recyclerView.SetLayoutManager(layoutManager);
             recyclerView.AddItemDecoration(new SimpleItemDecoration(this));
-
-            string url = GetString(Resource.String.data_url);
 
             // Show progress dialog
             ProgressDialog progressDialog = new ProgressDialog(this);
@@ -40,27 +39,38 @@ namespace JsonSample
             progressDialog.Show();
 
             // Fetch songs
-            List<Song> songs = await FetchSongs(url);
+            try
+            {
+                string url = GetString(Resource.String.data_url_github);
+                List<Song> songs = await FetchSongs(url);
 
-            // Update UI
-            recyclerView.SetAdapter(new JsonAdapter(songs));
-            progressDialog.Hide();
+                // Update UI
+                recyclerView.SetAdapter(new JsonAdapter(songs));
+            }
+            catch (Exception e)
+            {
+                Toast.MakeText(this, "Failed to download data: " + e.Message, ToastLength.Long).Show();
+            }
+            finally
+            {
+                progressDialog.Hide();
+            }
         }
 
         private async Task<List<Song>> FetchSongs(string url)
         {
             // Create an HTTP web request using the URL:
-            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create (new Uri (url));
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
             request.ContentType = "application/json";
             request.Method = "GET";
 
             // Send the request to the server and wait for the response:
-            using (WebResponse response = await request.GetResponseAsync ())
+            using (WebResponse response = await request.GetResponseAsync())
             {
                 // Get a stream representation of the HTTP web response:
-                using (Stream stream = response.GetResponseStream ())
+                using (Stream stream = response.GetResponseStream())
                 {
-                    JsonValue jsonDoc = await Task.Run (() => JsonObject.Load (stream));
+                    JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
                     List<Song> songs = JsonConvert.DeserializeObject<List<Song>>(jsonDoc.ToString());
                     return songs;
                 }
