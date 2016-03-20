4faeece51a6535b986af2e7f87e67f51b4801488
anaselhajjaji <elhajjaji.anas@gmail.com>
2016-03-20 21:18:26 +0000

    Add Tamarin.Forms ListView Sample

---
+++ Xamarin.Forms/ListViewSample/ListViewSample/Employee.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using Xamarin.Forms;
+
+namespace ListViewSample
+{
+    public class Employee
+    {
+        public string DisplayName {get; set;}
+    }
+}
+
---
+++ Xamarin.Forms/ListViewSample/ListViewSample/MyMainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using Xamarin.Forms;
+
+namespace ListViewSample
+{
+    public partial class MyMainPage : ContentPage
+    {
+        ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+
+        public MyMainPage()
+        {
+            InitializeComponent();
+
+            // Define the data source
+            EmployeeView.ItemsSource = employees;
+
+            // Populate the data source
+            PopulateDataSource();
+        }
+
+        private void PopulateDataSource() {
+            for (int i = 1; i < 100; i++)
+            {
+                employees.Add(new Employee{ DisplayName="Employee Number " + i});
+            }
+        }
+    }
+}
+
